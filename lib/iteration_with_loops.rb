a77c1d8ac0194fcce24c8c86b65d3e80ecd82ea0
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-26 23:43:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,15 +5,14 @@ while row_index < src.length do
   element_index = 0
   while element_index < src[row_index].length do
    if src[row_index][element_index].class == String
-     outer_results =  scr[row_index][element_index]
+     outer_results <<  scr[row_index][element_index]
    end
     element_index += 1
   end
   row_index += 1
 end
-
-  outer_results 
-
+  outer_results.join(" ")
 end
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  
